f47cbf177cdaeea4b2b74daed976e766614873f9
Mert Can Karsi <karsi.mertcan@gmail.com>
2021-06-30 01:28:22 +0300

    solved: Designer PDF Viewer
    https://www.hackerrank.com/challenges/designer-pdf-viewer/problem

---
+++ src/main/java/com/mckarsi/hackerrankjava/designerpdfviewer/Solution.java
@@ -0,0 +1,56 @@
+package com.mckarsi.hackerrankjava.designerpdfviewer;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        // Write your code here
+        int size = word.length();
+        int maxHeight = 1;
+        for(int i=0; i<size; i++) {
+            int c = word.charAt(i);
+            System.out.println(word.charAt(i)+" "+c);
+            int height = h.get(c-97);
+            if(height>maxHeight)
+                maxHeight = height;
+        }
+        return size * maxHeight;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
